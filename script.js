00a318011c6f3a22864c0d2ca317ac342b329c0c
Andrey <noonverto007@gmail.com>
2021-11-22 19:44:39 +0200

    js step 2

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const cells = document.querySelectorAll('.cell'),
-    massage = document.querySelector('.massage');
+    massage = document.querySelector('.massage'),
+    overley = document.querySelector('.overley'),
+    btnRestart = document.querySelector('.btn-restart'),
+    btnQuit = document.querySelector('.btn-quit');
+
 
 let currentTurn = 'Player 1';
 
@@ -43,10 +47,26 @@ function action() {
                 cell.removeEventListener('mouseleave', hoverOut);
                 cell.removeEventListener('click', action);
                 cell.style.cursor = 'not-allowed';
-            })
+                overley.classList.add('active');
+            });
             return;  
+        } else {
+            const res = Array.from(cells).every(cell => {
+                return cell.classList.length === 2;
+            });
+            if(res){
+                massage.innerText = 'It is a Tie';
+                cells.forEach(cell => {
+                    cell.removeEventListener('mouseenter', hoverIn);
+                    cell.removeEventListener('mouseleave', hoverOut);
+                    cell.removeEventListener('click', action);
+                    cell.style.cursor = 'not-allowed';
+                    overley.classList.add('active');
+                    cell.classList.add('higlight');
+                });
+                return;
+            }
         }
-
         currentTurn === 'Player 1' ? (currentTurn = 'Player 2') : (currentTurn = 'Player 1');
         massage.innerText = `${currentTurn}'s  Turn`;
 }
@@ -60,10 +80,26 @@ function hoverIn (){
     }
 }
 
-
 function hoverOut (){
     if(this.classList.contains('cross-hover') || this.classList.contains('circle-hover')) {
         this.classList.remove('cross-hover');
         this.classList.remove('circle-hover');
     }  
 }
+
+btnRestart.addEventListener('click', () => {
+        overley.classList.remove('active');
+        massage.innerText = "Player 1's Turn";
+        cells.forEach(cell => {   
+            cell.classList.remove('higlight');
+            cell.classList.remove('cross');  
+            cell.classList.remove('circle');  
+            cell.removeEventListener('click', action);
+            cell.addEventListener('click', action, {once: true});
+            cell.style.cursor = 'pointer';
+        })
+})
+
+btnQuit.addEventListener('click', () => {
+    window.close(); 
+})
